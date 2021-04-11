10c0829df50011b1340ed11bc6b35ad85b15cc89
Ashfak rahman <ashfakrahman@Ashfaks-MacBook-Pro.local>
2021-04-11 00:00:11 -0400

    test1

---
+++ com/bit/test/JenkinTest.java
@@ -0,0 +1,16 @@
+package com.bit.test;
+
+public class JenkinTest {
+
+	public static void main(String[] args) {
+		test1();
+		test2();
+
+	}
+ public static void test1() {
+	 System.out.println("print abcd");
+ }
+ public static void test2() {
+	 System.out.println("print xyz");
+ }
+}
